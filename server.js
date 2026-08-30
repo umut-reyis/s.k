ff195bd344e4ead3e60e32439e29a485c71c8b55
umut-reyis <ahmettacar6789@gmail.com>
2026-08-30 16:42:04 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+let servers = [
+    { id: 101, name: "Genel Lobi 1", host: "Sistem", players: 0, max: 20, inGame: false }
+];
+
+io.on('connection', (socket) => {
+    console.log(`Bir oyuncu bağlandı: ${socket.id}`);
+
+    socket.emit('server-list-update', servers);
+
+    socket.on('get-servers', () => {
+        socket.emit('server-list-update', servers);
+    });
+
+    socket.on('create-server', (data) => {
+        const newSrv = {
+            id: Date.now(),
+            name: data.serverName,
+            host: data.playerName,
+            players: 1,
+            max: 20,
+            inGame: false
+        };
+        servers.unshift(newSrv);
+        socket.join(newSrv.id);
+        socket.data.currentServerId = newSrv.id;
+        socket.data.playerName = data.playerName;
+        
+        io.emit('server-list-update', servers);
+        socket.emit('server-joined', { server: newSrv, isHost: true });
+    });
+
+    socket.on('join-server', (serverId) => {
+        const srv = servers.find(s => s.id === serverId);
+        if (srv && !srv.inGame && srv.players < srv.max) {
+            socket.join(srv.id);
+            socket.data.currentServerId = srv.id;
+            srv.players++;
+            io.emit('server-list-update', servers);
+            socket.emit('server-joined', { server: srv, isHost: false });
+        } else {
+            socket.emit('alert', 'Sunucu dolu veya halihazırda oyunda!');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (socket.data.currentServerId) {
+            const srv = servers.find(s => s.id === socket.data.currentServerId);
+            if (srv) {
+                srv.players = Math.max(0, srv.players - 1);
+                if (srv.players === 0 && srv.id !== 101) {
+                    servers = servers.filter(s => s.id !== srv.id);
+                }
+                io.emit('server-list-update', servers);
+            }
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor.`);
+});
